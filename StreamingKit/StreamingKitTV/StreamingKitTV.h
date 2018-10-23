a64e797deea6f3a4dcb9f2f2dd60fa8391778f78
Фучкин И.О <ifuchkin@rsb.ru>
2018-10-23 14:32:13 +0300

    TV

---
+++ StreamingKit/StreamingKitTV/StreamingKitTV.h
@@ -0,0 +1,13 @@
+//
+//  StreamingKitTV.h
+//  StreamingKitTV
+//
+//  Created by iMac on 23/10/2018.
+//  Copyright © 2018 Thong Nguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StreamingKitTV : NSObject
+
+@end
---
+++ StreamingKit/StreamingKitTV/StreamingKitTV.m
@@ -0,0 +1,13 @@
+//
+//  StreamingKitTV.m
+//  StreamingKitTV
+//
+//  Created by iMac on 23/10/2018.
+//  Copyright © 2018 Thong Nguyen. All rights reserved.
+//
+
+#import "StreamingKitTV.h"
+
+@implementation StreamingKitTV
+
+@end
